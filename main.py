3d387093f2c4cd169b59d2d01abb857425d41535
Deepika Sharma <deepikaasharma@outlook.com>
2020-08-27 04:07:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+"""Write a function called remove_elem_at_zero that takes a list as an argument. The function should return the list with the first element removed."""
+
+def remove_elem_at_zero(lst):
+  del lst[0]
+  return lst
